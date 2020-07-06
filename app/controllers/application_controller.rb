b544770c7f816a7d2e481ed8485b659850d4c40f
MoiseDeCarvalho <moise@moise.it>
2020-07-07 00:02:08 +0200

    sistemato controntroller principale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	helper :current_user
+
+	def current_user
+    	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  	end
 end
