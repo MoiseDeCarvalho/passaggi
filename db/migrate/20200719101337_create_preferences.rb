4f57166e4c1494914c5b60efad688ce2981277f2
MoiseDeCarvalho <moise@moise.it>
2020-10-08 08:24:41 +0200

    corretto migration alter column preferences

--- db/migrate/20200719101337_create_preferences.rb
+++ db/migrate/20200719101337_create_preferences.rb
@@ -2,10 +2,10 @@ class CreatePreferences < ActiveRecord::Migration[6.0]
   def change
     create_table :preferences do |t|
       t.integer :user_id
-      t.boolean :conversation
-      t.boolean :music
-      t.boolean :smoke
-      t.boolean :animal
+      t.integer :conversation
+      t.integer :music
+      t.integer :smoke
+      t.integer :animal
 
       t.timestamps
     end
--- db/migrate/20200719132906_change_conversation_to_be_integer_in_preferences.rb
+++
@@ -1,8 +0,0 @@
-class ChangeConversationToBeIntegerInPreferences < ActiveRecord::Migration[6.0]
-  def change
-  	  change_column :preferences, :conversation, :integer
-  	  change_column :preferences, :music, :integer
-  	  change_column :preferences, :smoke, :integer
-  	  change_column :preferences, :animal, :integer
-  end
-end
